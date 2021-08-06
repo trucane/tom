97c84aa22068ee9fb55adad8170ef3da3cb208c6
trucane <trufashionista2013@gmail.com>
2021-08-06 02:05:59 -0500

    user profile

--- src/fed-src/components/Users/userProfile.js
+++ src/fed-src/components/Users/userProfile.js
@@ -20,14 +20,43 @@ const UserProfile = (props) => {
 
     return(
         <Fragment>
-            Profile
-            
+            {user && (
+
+            <div className='userProfile'>
+                <div className="profile-header">
+                    <div className="profile-link">
+                        <Link to='/'>
+                            / users
+                        </Link>
+                    </div>
+                    <div className='profile-username'>{user.username}</div>
+                </div>
+
+                <div className="profile-details-container">
+                    <div className='profile-image'>
+                        <img src={userimage} alt={user.name} />
+                    </div>
+                    <div className ='profile-details' >
+                        <div className='profileusername'>{user.name}</div>
+                        <div className='profileusername'>{user.email}</div>
+                        <div className='profileaddress'>{user.address.city}</div>
+                    </div>
+                </div>
+            </div>
+            )}
         </Fragment>
     )
 }
 
 
-
+// UserProfile.propTypes = {
+//     data: PropTypes.shape({
+//         id: PropTypes.number.isRequired,
+//         name: PropTypes.string.isRequired,
+//         username: PropTypes.string.isRequired,
+//         userImageURL: PropTypes.string,
+//     }),
+// };
 
 
 export default UserProfile;
